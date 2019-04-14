a1f975bfb9200a8fd15d723c8ef640675ca7d0e1
Colton B <clb4416@g.rit.edu>
2019-04-13 22:35:10 -0400

    Initial push

--- SpaceStationSystem/Mega.cs
+++ SpaceStationSystem/Mega.cs
@@ -12,16 +12,21 @@ namespace SpaceStationSystem
         private bool _heavyFloor;
         private bool _water;
 
+        // Do they need oxygen to breathe?
         public bool Oxygen
         {
             get => _oxygen;
             set => _oxygen = true;
         }
+
+        // Do they need Heavy Floor to walk on?
         public bool HeavyFloor
         {
             get => _heavyFloor;
             set => _heavyFloor = true;
         }
+
+        // Do they need water to breathe?
         public bool Water
         {
             get => _water;
